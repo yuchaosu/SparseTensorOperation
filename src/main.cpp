13f0a0eaaad222db6f1f966c1cc4d9dabe1f0a16
Yuchao Su <su.yuchao@outlook.com>
2024-11-13 19:23:12 -0500

    update the compile label for openMP

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,60 @@ SparseTensorCOO tensorElementWiseMultiply(const SparseTensorCOO& A,
     sortedA.sort();
     sortedB.sort();
 
+    size_t sizeA = sortedA.values.size();
+    size_t sizeB = sortedB.values.size();
+
+    // Parallel merging is complex due to data dependencies.
+    // Here we attempt to parallelize using OpenMP tasks.
+
+    #ifdef USE_OPENMP
+    std::vector<int> C_coords1, C_coords2, C_coords3;
+    std::vector<double> C_values;
+
+    #pragma omp parallel
+    {
+        std::vector<int> local_coords1, local_coords2, local_coords3;
+        std::vector<double> local_values;
+
+        #pragma omp single nowait
+        {
+            size_t i = 0, j = 0;
+            while (i < sizeA && j < sizeB) {
+                int ai = sortedA.coords1[i];
+                int aj = sortedA.coords2[i];
+                int ak = sortedA.coords3[i];
+
+                int bi = sortedB.coords1[j];
+                int bj = sortedB.coords2[j];
+                int bk = sortedB.coords3[j];
+
+                if (ai == bi && aj == bj && ak == bk) {
+                    double valA = sortedA.values[i];
+                    double valB = sortedB.values[j];
+
+                    #pragma omp task firstprivate(ai, aj, ak, valA, valB)
+                    {
+                        double val = valA * valB;
+
+                        #pragma omp critical
+                        {
+                            C.addElement(ai, aj, ak, val);
+                        }
+                    }
+                    ++i;
+                    ++j;
+                } else if (ai < bi ||
+                           (ai == bi && (aj < bj || (aj == bj && ak < bk)))) {
+                    ++i;
+                } else {
+                    ++j;
+                }
+            }
+        }
+    }
+    #else
     size_t i = 0, j = 0;
-    while (i < sortedA.values.size() && j < sortedB.values.size())
+    while (i < sizeA && j < sizeB)
     {
         int ai = sortedA.coords1[i];
         int aj = sortedA.coords2[i];
@@ -37,16 +89,17 @@ SparseTensorCOO tensorElementWiseMultiply(const SparseTensorCOO& A,
             ++i;
             ++j;
         } else if (ai < bi ||
-                   (ai == bi && (aj < bj || (ai == bj && ak < bk)))) {
+                   (ai == bi && (aj < bj || (aj == bj && ak < bk)))) {
             ++i;
         } else {
             ++j;
         }
     }
-    
-    return C;
+    #endif
 
+    return C;
 }
+
 
 SparseTensorHiCOO tensorElementWiseMultiply(const SparseTensorHiCOO& A,
                                             const SparseTensorHiCOO& B) {
@@ -68,7 +121,53 @@ SparseTensorHiCOO tensorElementWiseMultiply(const SparseTensorHiCOO& A,
     }
 
     // Perform element-wise multiplication
-    for (size_t idx = 0; idx < A.values.size(); ++idx) {
+    std::vector<int> local_blockCoord1, local_blockCoord2, local_blockCoord3;
+    std::vector<int> local_elemCoord1, local_elemCoord2, local_elemCoord3;
+    std::vector<double> local_values;
+
+    size_t n = A.values.size();
+
+    #ifdef USE_OPENMP
+    #pragma omp parallel
+    {
+        std::vector<int> thread_blockCoord1, thread_blockCoord2, thread_blockCoord3;
+        std::vector<int> thread_elemCoord1, thread_elemCoord2, thread_elemCoord3;
+        std::vector<double> thread_values;
+
+        #pragma omp for schedule(static)
+        for (size_t idx = 0; idx < n; ++idx) {
+            auto key = std::make_tuple(
+                A.blockCoord1[idx], A.blockCoord2[idx], A.blockCoord3[idx],
+                A.elemCoord1[idx], A.elemCoord2[idx], A.elemCoord3[idx]);
+
+            auto it = BElements.find(key);
+            if (it != BElements.end()) {
+                double val = A.values[idx] * it->second;
+                if (val != 0.0) {
+                    thread_blockCoord1.push_back(A.blockCoord1[idx]);
+                    thread_blockCoord2.push_back(A.blockCoord2[idx]);
+                    thread_blockCoord3.push_back(A.blockCoord3[idx]);
+                    thread_elemCoord1.push_back(A.elemCoord1[idx]);
+                    thread_elemCoord2.push_back(A.elemCoord2[idx]);
+                    thread_elemCoord3.push_back(A.elemCoord3[idx]);
+                    thread_values.push_back(val);
+                }
+            }
+        }
+
+        #pragma omp critical
+        {
+            local_blockCoord1.insert(local_blockCoord1.end(), thread_blockCoord1.begin(), thread_blockCoord1.end());
+            local_blockCoord2.insert(local_blockCoord2.end(), thread_blockCoord2.begin(), thread_blockCoord2.end());
+            local_blockCoord3.insert(local_blockCoord3.end(), thread_blockCoord3.begin(), thread_blockCoord3.end());
+            local_elemCoord1.insert(local_elemCoord1.end(), thread_elemCoord1.begin(), thread_elemCoord1.end());
+            local_elemCoord2.insert(local_elemCoord2.end(), thread_elemCoord2.begin(), thread_elemCoord2.end());
+            local_elemCoord3.insert(local_elemCoord3.end(), thread_elemCoord3.begin(), thread_elemCoord3.end());
+            local_values.insert(local_values.end(), thread_values.begin(), thread_values.end());
+        }
+    }
+    #else
+    for (size_t idx = 0; idx < n; ++idx) {
         auto key = std::make_tuple(
             A.blockCoord1[idx], A.blockCoord2[idx], A.blockCoord3[idx],
             A.elemCoord1[idx], A.elemCoord2[idx], A.elemCoord3[idx]);
@@ -77,19 +176,28 @@ SparseTensorHiCOO tensorElementWiseMultiply(const SparseTensorHiCOO& A,
         if (it != BElements.end()) {
             double val = A.values[idx] * it->second;
             if (val != 0.0) {
-                C.blockCoord1.emplace_back(A.blockCoord1[idx]);
-                C.blockCoord2.emplace_back(A.blockCoord2[idx]);
-                C.blockCoord3.emplace_back(A.blockCoord3[idx]);
-                C.elemCoord1.emplace_back(A.elemCoord1[idx]);
-                C.elemCoord2.emplace_back(A.elemCoord2[idx]);
-                C.elemCoord3.emplace_back(A.elemCoord3[idx]);
-                C.values.emplace_back(val);
+                local_blockCoord1.push_back(A.blockCoord1[idx]);
+                local_blockCoord2.push_back(A.blockCoord2[idx]);
+                local_blockCoord3.push_back(A.blockCoord3[idx]);
+                local_elemCoord1.push_back(A.elemCoord1[idx]);
+                local_elemCoord2.push_back(A.elemCoord2[idx]);
+                local_elemCoord3.push_back(A.elemCoord3[idx]);
+                local_values.push_back(val);
             }
         }
     }
+    #endif
+
+    // Assign results to C
+    C.blockCoord1 = std::move(local_blockCoord1);
+    C.blockCoord2 = std::move(local_blockCoord2);
+    C.blockCoord3 = std::move(local_blockCoord3);
+    C.elemCoord1 = std::move(local_elemCoord1);
+    C.elemCoord2 = std::move(local_elemCoord2);
+    C.elemCoord3 = std::move(local_elemCoord3);
+    C.values = std::move(local_values);
 
     return C;
-
 }
 
 SparseTensorCOO tensorTimesVector(const SparseTensorCOO& A,
@@ -112,11 +220,12 @@ SparseTensorCOO tensorTimesVector(const SparseTensorCOO& A,
 
     std::map<std::tuple<int, int, int>, double> outElements;
 
+    #ifdef USE_OPENMP
     #pragma omp parallel
     {
         std::map<std::tuple<int, int, int>, double> localOutElements;
 
-        #pragma omp for nowait
+        #pragma omp for nowait schedule(static)
         for (size_t i = 0; i < nnz; ++i){
             double val = A.values[i];
             int idx1 = A.coords1[i];
@@ -154,10 +263,40 @@ SparseTensorCOO tensorTimesVector(const SparseTensorCOO& A,
             {
                 outElements[kv.first] += kv.second;
             }
-            
         }
-
     }
+    #else
+    for (size_t i = 0; i < nnz; ++i){
+        double val = A.values[i];
+        int idx1 = A.coords1[i];
+        int idx2 = A.coords2[i];
+        int idx3 = A.coords3[i];
+
+        double vecVal = 0.0;
+        if (mode == 1 && idx1 < static_cast<int>(v.size()))
+            vecVal = v[idx1];
+        else if (mode == 2 && idx2 < static_cast<int>(v.size()))
+            vecVal = v[idx2];
+        else if (mode == 3 && idx3 < static_cast<int>(v.size()))
+            vecVal = v[idx3];
+        else 
+            continue;
+
+        val *= vecVal;
+
+        int outIdx1 = idx1, outIdx2 = idx2, outIdx3 = idx3;
+        if (mode == 1)
+            outIdx1 = 0;
+        else if (mode == 2)
+            outIdx2 = 0;
+        else if (mode == 3)
+            outIdx3 = 0;
+
+        std::tuple<int, int, int> key = std::make_tuple(outIdx1, outIdx2, outIdx3);
+
+        outElements[key] += val;
+    }
+    #endif
 
     for (const auto& kv : outElements)
     {
@@ -171,19 +310,15 @@ SparseTensorCOO tensorTimesVector(const SparseTensorCOO& A,
     }
     
     return C;
-    
 }
 
-SparseTensorHiCOO tensorTimesVector(const SparseTensorHiCOO& A,
-                                            const std::vector<double>& v, int mode) {
 
-    
-    // mode is 1-based index (1,2,3)
+SparseTensorHiCOO tensorTimesVector(const SparseTensorHiCOO& A,
+                                    const std::vector<double>& v, int mode) {
     if (mode < 1 || mode > 3) {
         throw std::invalid_argument("Mode must be between 1 and 3.");
     }
 
-    // Determine the dimensions of the output tensor
     int m1 = A.dim1, m2 = A.dim2, m3 = A.dim3;
     int out_m1 = m1, out_m2 = m2, out_m3 = m3;
     if (mode == 1)
@@ -199,12 +334,12 @@ SparseTensorHiCOO tensorTimesVector(const SparseTensorHiCOO& A,
 
     size_t nnz = A.values.size();
 
-    // Parallelize over non-zero elements
+    #ifdef USE_OPENMP
     #pragma omp parallel
     {
         std::map<std::tuple<int, int, int, int, int, int>, double> local_out_elements;
 
-        #pragma omp for nowait
+        #pragma omp for nowait schedule(static)
         for (size_t idx = 0; idx < nnz; ++idx) {
             double val = A.values[idx];
 
@@ -265,6 +400,59 @@ SparseTensorHiCOO tensorTimesVector(const SparseTensorHiCOO& A,
             }
         }
     }
+    #else
+    for (size_t idx = 0; idx < nnz; ++idx) {
+        double val = A.values[idx];
+
+        int blockI = A.blockCoord1[idx];
+        int blockJ = A.blockCoord2[idx];
+        int blockK = A.blockCoord3[idx];
+
+        int elemI = A.elemCoord1[idx];
+        int elemJ = A.elemCoord2[idx];
+        int elemK = A.elemCoord3[idx];
+
+        int global_i = blockI * A.blockSize + elemI;
+        int global_j = blockJ * A.blockSize + elemJ;
+        int global_k = blockK * A.blockSize + elemK;
+
+        double vec_val = 0.0;
+        if (mode == 1 && global_i < static_cast<int>(v.size()))
+            vec_val = v[global_i];
+        else if (mode == 2 && global_j < static_cast<int>(v.size()))
+            vec_val = v[global_j];
+        else if (mode == 3 && global_k < static_cast<int>(v.size()))
+            vec_val = v[global_k];
+        else
+            continue; // Index out of bounds
+
+        val *= vec_val;
+
+        // Reduce along the mode
+        int out_global_i = global_i, out_global_j = global_j, out_global_k = global_k;
+        if (mode == 1)
+            out_global_i = 0;
+        else if (mode == 2)
+            out_global_j = 0;
+        else if (mode == 3)
+            out_global_k = 0;
+
+        int out_block_i = out_global_i / A.blockSize;
+        int out_block_j = out_global_j / A.blockSize;
+        int out_block_k = out_global_k / A.blockSize;
+
+        int out_elem_i = out_global_i % A.blockSize;
+        int out_elem_j = out_global_j % A.blockSize;
+        int out_elem_k = out_global_k % A.blockSize;
+
+        auto key = std::make_tuple(
+            out_block_i, out_block_j, out_block_k,
+            out_elem_i, out_elem_j, out_elem_k);
+
+        // Accumulate
+        out_elements[key] += val;
+    }
+    #endif
 
     // Convert map to HiCOOTensor
     for (const auto& kv : out_elements) {
